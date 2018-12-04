811efd237c393b3995c6fab0b3fbd22c2738c51e
rschmid8 <44205179+rschmid8@users.noreply.github.com>
2018-12-03 19:11:16 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1sQUKgk12F1IBEsl7pw0VincHnUSusVVdtIXcRzt8cCU/edit#gid=0';
